c6caf7c64ec5497b10f06a8b59ccabacd42bc2c4
Marat Ikhsanov <m.ihsanov2009@yandex.ru>
2023-03-21 15:29:19 +0300

    Delete frontend/src directory

--- frontend/src/components/home/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import styles from "./home.module.scss"
-
-const Home = () => {
-  return <div>Стартовая страница УЗ ИГиРГИ</div>
-}
-
-export default Home
--- frontend/src/utils/errors.ts
+++
@@ -1,7 +0,0 @@
-export const CODES = {
-  SERVER_ERR: "server_err",
-}
-
-export const MESSAGES = {
-  [CODES.SERVER_ERR]: "Что-то пошло не так. Попробуйте еще раз через 1 минуту.",
-}
